ae9df4189e358e125ee8d5747f5d63906d5aa6a3
Alexandr Ischenko <alexxjc@gmail.com>
2016-06-14 01:06:52 +0300

    Having dealt with one-way and two-way binding.

--- js/common.js
+++ js/common.js
@@ -1,7 +1,18 @@
 var myAngularApp = angular.module('angularApp', []);
 var  model = "Hellow World";
+
+var list = {
+            user: "User1",
+            courses: [{ name: "HTML, CSS", passed: false },
+                { name: "JavaScript Essential", passed: false },
+                { name: "JavaScript Advanced", passed: false },
+                { name: "HTML5 &amp; CSS3", passed: false },
+                { name: "AngularJS", passed: false }]
+        };
+
 myAngularApp.controller('messageCtrl', function ($scope) {
 	$scope.message = model;
+	$scope.data = list;
 
 	$scope.clickHandler = function() {
 		
@@ -13,4 +24,20 @@ myAngularApp.controller('messageCtrl', function ($scope) {
 		$scope.text = '';
 	};
 
+	$scope.addTask = function () {
+		
+				$scope.data.courses.push({
+				name: $scope.dataTask,
+				passed: false
+			});
+	
+		$scope.dataTask = '';
+	}
+	$scope.showText = function (passed) {
+    return passed ? "Да" : "Нет";
+  }
+
+  $scope.setStyle = function (passed) {
+      return passed ? "color:green" : "color:red; font-weight: bold";
+  }
 })
